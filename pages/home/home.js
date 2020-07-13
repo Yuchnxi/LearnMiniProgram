efaf458656a5ab6fd1f3e84b7a86c42347d236df
yuchenxi6 <yuchenxi748521@163.com>
2020-07-14 02:04:53 +0800

    组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    counter: 0
   },
 
   /**
@@ -62,5 +62,26 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  handleIncrement() {
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleTabClick(event) {
+    console.log(event)
+  },
+  handleIncrementCpn() {
+    // 最终目的：修改 my-sel 中的数据
+    // 1.获取组件对象
+    const my_sel = this.selectComponent('#sel-id')
+
+    // 2.通过setDate 修改组件中的数据（不合理）
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 20
+    // })
+
+    // 3.通过方法对数据进行修改
+    my_sel.incrementCounter(10)
   }
 })
